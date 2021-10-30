b54b0563a18520105e5f7dbe7d03dd7ab302e2cd
Unique Maharjan <unique.maharzn@gmail.com>
2021-10-31 00:34:07 +0545

    Added some feature and modification

--- note/admin.py
+++ note/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import SubjectList,SubjectNote
+from .models import SubjectList,SubjectNote, UserProfile
 # Register your models here.
 
 admin.site.register(SubjectNote)
 admin.site.register(SubjectList)
+admin.site.register(UserProfile)
--- note/models.py
+++ note/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.deletion import CASCADE
 # Create your models here.
 class SubjectList(models.Model):
     name = models.CharField(max_length=200)
@@ -12,7 +13,7 @@ class SubjectNote(models.Model):
     topic = models.ForeignKey(SubjectList, on_delete= models.CASCADE)
     name = models.CharField(max_length=200)
     body = models.TextField(null = True,blank=True)
-    file_upload = models.FileField(upload_to = None,null=True,blank=True)
+    file_upload = models.FileField(upload_to = "files",null=True,blank=True)
     created = models.DateTimeField(auto_now_add= True)
     updated = models.DateTimeField(auto_now=True)
     
@@ -20,3 +21,10 @@ class SubjectNote(models.Model):
         ordering = ['-updated','-created']
     def __str__(self):
         return self.name
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete= models.CASCADE)
+    profile_pic = models.ImageField(upload_to = "profile")
+
+    def __str__(self):
+        return f'Profile of {self.user}'
