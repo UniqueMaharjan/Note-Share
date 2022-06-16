dd40aafc79c7eb602565b47a6bd2a444452aebda
Neeraj319 <itskop520@gmail.com>
2022-06-16 12:53:13 +0545

    [added] custom user manager class

--- note/models.py
+++ note/models.py
@@ -1,17 +1,56 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from typing import List
+from django.contrib.auth.base_user import BaseUserManager
 
 # Create your models here.
-class User(AbstractUser):
-    name = models.CharField(max_length=200,null=True,blank=True)
-    email = models.EmailField(unique=True,null=True)
-    bio = models.TextField(null=True,blank=True)
-    avatar = models.ImageField(default = "images/avatar.svg",upload_to = "images",null = True)
+class CustomUserManager(BaseUserManager):
+    """
+    Custom user model manager where email is the unique identifiers
+    for authentication instead of usernames.
+    """
 
-    USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = []
+    def create_user(self, email, password, **extra_fields):
+        """
+        Create and save a User with the given email and password.
+        """
+        if not email:
+            raise ValueError("The Email must be set")
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save()
+        return user
+
+    def create_superuser(self, email, password, **extra_fields):
+        """
+        Create and save a SuperUser with the given email and password.
+        """
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+        extra_fields.setdefault("is_active", True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser must have is_staff=True.")
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser must have is_superuser=True.")
+        return self.create_user(email, password, **extra_fields)
+
+
+class User(AbstractUser):
+    name = models.CharField(max_length=200, null=True, blank=True)
+    email = models.EmailField(unique=True)
+    bio = models.TextField(null=True, blank=True)
+    avatar = models.ImageField(
+        default="images/avatar.svg", upload_to="images", null=True
+    )
+    REQUIRED_FIELDS: List[str] = []
+    USERNAME_FIELD = "email"
+    objects = CustomUserManager()
+
     def __str__(self):
         return self.email
+
 
 class SubjectList(models.Model):
     name = models.CharField(max_length=200)
@@ -19,22 +58,18 @@ class SubjectList(models.Model):
     def __str__(self):
         return self.name
 
+
 class SubjectNote(models.Model):
-    host= models.ForeignKey(User, on_delete= models.SET_NULL,null=True)
-    topic = models.ForeignKey(SubjectList, on_delete= models.CASCADE)
+    host = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    topic = models.ForeignKey(SubjectList, on_delete=models.CASCADE)
     name = models.CharField(max_length=200)
-    body = models.TextField(null = True,blank=True)
-    file_upload = models.FileField(upload_to = "files",null=True,blank=True)
-    created = models.DateTimeField(auto_now_add= True)
+    body = models.TextField(null=True, blank=True)
+    file_upload = models.FileField(upload_to="files", null=True, blank=True)
+    created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
-    
+
     class Meta:
-        ordering = ['-updated','-created']
+        ordering = ["-updated", "-created"]
+
     def __str__(self):
         return self.name
-    # @property
-    # def file_name(self):
-    #     file = self.file_upload.find('/')
-    #     filename = self.file_upload[file+1:]
-    #     return filename
-
--- note/views.py
+++ note/views.py
@@ -1,149 +1,158 @@
-from django.http.response import Http404
-from django.shortcuts import render,redirect
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.db.models import Q
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth import authenticate,login,logout
+from django.contrib.auth import authenticate, login, logout
 
-from .models import SubjectNote,SubjectList,User
-from .forms import noteForm,listForm,MyUserCreationForm,MyUserForm
-import os
-from django.conf import settings
+from .models import SubjectNote, SubjectList, User
+from .forms import noteForm, MyUserCreationForm, MyUserForm
+
 
 def login_user(request):
-    page = 'login'
+    page = "login"
     if request.user.is_authenticated:
-        return redirect('home')
+        return redirect("home")
     if request.method == "POST":
-        email = request.POST.get('email').lower()
-        password = request.POST.get('password')
+        email = request.POST.get("email").lower()
+        password = request.POST.get("password")
         try:
-            user = User.objects.get(email = email)
+            user = User.objects.get(email=email)
         except:
-            messages.error(request,'User doesnot exit!!')
-        user = authenticate(request, email = email, password = password)
-    
+            messages.error(request, "User doesnot exit!!")
+        user = authenticate(request, email=email, password=password)
+
         if user != None:
-            login(request,user)
-            return redirect('home')
+            login(request, user)
+            return redirect("home")
         else:
-            messages.error(request,"User or Password doesn't exist!!")
-    content = {'page':page}
-    return render(request,'note/login-register.html',content)
+            messages.error(request, "User or Password doesn't exist!!")
+    content = {"page": page}
+    return render(request, "note/login-register.html", content)
+
 
 def logout_user(request):
     logout(request)
-    return redirect('home')
+    return redirect("home")
+
 
 def register_user(request):
     if request.user.is_authenticated:
-        return redirect('home')
+        return redirect("home")
     form = MyUserCreationForm()
     if request.method == "POST":
         form = MyUserCreationForm(request.POST)
         if form.is_valid():
-            user = form.save(commit = False)
+            user = form.save(commit=False)
             user.username = user.username.lower()
             user.save()
-            login(request,user)
-            return redirect('home')
+            login(request, user)
+            return redirect("home")
         else:
-            messages.error(request,'Please Checkout your passwords.... Try to implement strong one . .')
-    content = {
-        'form':form
-    }
-    return render(request,'note/login-register.html',content)
+            messages.error(
+                request,
+                "Please Checkout your passwords.... Try to implement strong one . .",
+            )
+    content = {"form": form}
+    return render(request, "note/login-register.html", content)
+
 
 def home(request):
-    q = request.GET.get('q') if request.GET.get('q') != None else ''
+    q = request.GET.get("q") if request.GET.get("q") != None else ""
     topics = SubjectList.objects.all()
     notes = SubjectNote.objects.filter(
-        Q(topic__name__icontains = q)|
-        Q(name__icontains = q)|
-        Q(host__username__icontains = q)
+        Q(topic__name__icontains=q)
+        | Q(name__icontains=q)
+        | Q(host__username__icontains=q)
     )
     note_count = notes.count()
     content = {
-        'note':notes,
-        'topic':topics,
-        'count':note_count,
+        "note": notes,
+        "topic": topics,
+        "count": note_count,
     }
-    return render(request,'note/home.html',content)
+    return render(request, "note/home.html", content)
 
-@login_required(login_url='login')
-def noteDetails(request,pk):
-    notes = SubjectNote.objects.get(id = pk)
+
+@login_required(login_url="login")
+def noteDetails(request, pk):
+    notes = SubjectNote.objects.get(id=pk)
     content = {
-        'note':notes,
+        "note": notes,
     }
-    return render(request,'note/note.html',content)
+    return render(request, "note/note.html", content)
 
-@login_required(login_url='login')
+
+@login_required(login_url="login")
 def addNote(request):
     form = noteForm()
     topic = SubjectList.objects.all()
     if request.method == "POST":
-        topic_name = request.POST.get('topic')
-        topic,created = SubjectList.objects.get_or_create(name = topic_name)
+        topic_name = request.POST.get("topic")
+        topic, created = SubjectList.objects.get_or_create(name=topic_name)
 
         SubjectNote.objects.create(
-            host = request.user,
-            topic = topic,
-            name = request.POST.get('name'),
-            body = request.POST.get('body'),
-            file_upload = request.FILES.get('file_upload')
+            host=request.user,
+            topic=topic,
+            name=request.POST.get("name"),
+            body=request.POST.get("body"),
+            file_upload=request.FILES.get("file_upload"),
         )
-        return redirect('home')
-    return render(request,'note/create.html',{'form':form,'topic':topic})
+        return redirect("home")
+    return render(request, "note/create.html", {"form": form, "topic": topic})
 
-@login_required(login_url='login')
-def editNote(request,pk):
+
+@login_required(login_url="login")
+def editNote(request, pk):
     note = SubjectNote.objects.get(id=pk)
     form = noteForm(instance=note)
     topic = SubjectList.objects.all()
     if request.user != note.host:
         return HttpResponse("You are not allowed here!!")
-    
-    if request.method == "POST":
-        topic_name = request.POST.get('topic')
-        topic,created = SubjectList.objects.get_or_create(name = topic_name)
-        note.name = request.POST.get('name')
-        note.topic = topic
-        note.body = request.POST.get('body')
-        note.file_upload = request.FILES.get('file_upload')
-        note.save()
-        return redirect('home')
-    return render(request,'note/create.html',{"form":form,"topic":topic})
 
-@login_required(login_url='login')
-def deleteNote(request,pk):
+    if request.method == "POST":
+        topic_name = request.POST.get("topic")
+        topic, created = SubjectList.objects.get_or_create(name=topic_name)
+        note.name = request.POST.get("name")
+        note.topic = topic
+        note.body = request.POST.get("body")
+        note.file_upload = request.FILES.get("file_upload")
+        note.save()
+        return redirect("home")
+    return render(request, "note/create.html", {"form": form, "topic": topic})
+
+
+@login_required(login_url="login")
+def deleteNote(request, pk):
     note = SubjectNote.objects.get(id=pk)
     if request.method == "POST":
         note.delete()
-        return redirect('home')
-    return render(request,'note/delete.html',{'obj':note})
+        return redirect("home")
+    return render(request, "note/delete.html", {"obj": note})
 
 
-
-def UserProfile(request,pk):
-    user = User.objects.get(id = pk)
+def UserProfile(request, pk):
+    user = User.objects.get(id=pk)
     note = user.subjectnote_set.all()
-    context = {'user':user,'note':note}
-    return render(request,'note/user-profile.html',context)
+    context = {"user": user, "note": note}
+    return render(request, "note/user-profile.html", context)
+
 
 def updateUser(request):
-    user = User.objects.get(email = request.user.email)
-    form = MyUserForm(instance = user)
+    user = User.objects.get(email=request.user.email)
+    form = MyUserForm(instance=user)
     if request.method == "POST":
-        form = MyUserForm(request.POST,request.FILES,instance=user)
+        form = MyUserForm(request.POST, request.FILES, instance=user)
         if form.is_valid():
             form.save()
-            return redirect('home')
-    context = {'form':form,}
-    return render(request,'note/update-user.html',context)
+            return redirect("home")
+    context = {
+        "form": form,
+    }
+    return render(request, "note/update-user.html", context)
+
 
 def topicsPage(request):
-    q = request.GET.get('q') if request.GET.get('q') != None else ''
-    topic = SubjectList.objects.filter(name__icontains = q)
-    return render(request,'note/topics.html',{'topics':topic})
+    q = request.GET.get("q") if request.GET.get("q") != None else ""
+    topic = SubjectList.objects.filter(name__icontains=q)
+    return render(request, "note/topics.html", {"topics": topic})
